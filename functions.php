a88c35a09f403e446b1e917a16e629ed1730d5d0
Paul Ryley <paul@geminilabs.io>
2017-02-16 14:44:40 -0400

    verify WP version

--- functions.php
+++ functions.php
@@ -2,10 +2,28 @@
 
 require_once __DIR__ . '/autoload.php';
 
+global $wp_version;
+
 defined( 'DEV' ) || define( 'DEV', true );
 
+if( version_compare( '5.6.0', phpversion(), '>=' )) {
+	wp_die(
+		__( 'You must be using PHP 5.6.0 or greater.', 'castor' ),
+		__( 'Invalid PHP version', 'castor' )
+	);
+}
+
+if( version_compare( '4.7.0', $wp_version, '>=' )) {
+	wp_die(
+		__( 'You must be using WordPress 4.7.0 or greater.', 'castor' ),
+		__( 'Invalid WordPress version', 'castor' )
+	);
+}
+
 if( is_customize_preview() && filter_input( INPUT_GET, 'theme' )) {
-	wp_die( __( 'Theme must be activated prior to using the customizer.', 'castor' ));
+	wp_die(
+		__( 'Theme must be activated prior to using the customizer.', 'castor' )
+	);
 }
 
 if( basename( $stylesheet = get_option( 'template' )) !== 'templates' ) {
