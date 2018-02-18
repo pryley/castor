0d1c3d67676295f7e943b1a552dc013c0f9700b7
Paul Ryley <paul@geminilabs.io>
2018-02-18 14:43:43 -0700

    fix comments template

--- templates/single.php
+++ templates/single.php
@@ -4,5 +4,5 @@ while( have_posts() ) :
 	the_post();
 	Template::load( 'partials/article', get_post_type() );
 	Template::load( 'partials/prev-next', get_post_type() );
-	comments_template( '/templates/sections/comments.php' );
+	comments_template( '/templates/partials/comments.php' );
 endwhile;
