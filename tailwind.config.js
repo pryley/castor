98cd095c57bc8ace909840f741e52994683fcb2e
Paul Ryley <paul@geminilabs.io>
2019-11-06 23:35:27 +0100

    update dependencies and webpack

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,11 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   plugins: [
     require('@tailwindcss/custom-forms'), // https://tailwindcss-custom-forms.netlify.com/
+    require('tailwindcss-bg-alpha')(),
   ],
   theme: {
+    // alphaColors: ['blue.500'],
+    // alphaValues: [.1, .2, .3, .4, .5, .75],
     extend: {
       fontFamily: {
         sans: [...defaultTheme.fontFamily.sans],
